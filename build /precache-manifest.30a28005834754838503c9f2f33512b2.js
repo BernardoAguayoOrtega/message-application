273de56cb02d48f9a621c55dbb0050a0f06d68a5
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-09-05 14:33:28 -0500

    finish

---
+++ build /precache-manifest.30a28005834754838503c9f2f33512b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7ca90ccfcc26e81e84608cedb5cb1f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b450e9fd3e2ea9ec10b7",
+    "url": "/static/css/main.86bb5d5b.chunk.css"
+  },
+  {
+    "revision": "9df79995aa3fe669b473",
+    "url": "/static/js/2.ef2ce2cb.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.ef2ce2cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b450e9fd3e2ea9ec10b7",
+    "url": "/static/js/main.a3582f80.chunk.js"
+  },
+  {
+    "revision": "5ab11b5882991a69e9c9",
+    "url": "/static/js/runtime-main.78985f2f.js"
+  }
+]);
