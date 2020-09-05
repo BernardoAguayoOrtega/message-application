e40ebd77aa6c3c52a8139ac81c6b7fcf18f2220c
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-09-04 21:20:30 -0500

    add materiaul ui components to Message functional component

--- src/Components/Message/index.js
+++ src/Components/Message/index.js
@@ -1,11 +1,17 @@
 //import react
 import React from 'react';
+//import material design components
+import { Card, CardContent, Typography } from '@material-ui/core';
 
 //create and export functional component
 export const Message = ({ username, text }) => {
 	return (
-		<>
-			<h2>{`${username}: ${text}`}</h2>
-		</>
+		<Card>
+			<CardContent>
+				<Typography color='white' variant='h5' component='h2'>
+					{`${username}: ${text}`}
+				</Typography>
+			</CardContent>
+		</Card>
 	);
 };
