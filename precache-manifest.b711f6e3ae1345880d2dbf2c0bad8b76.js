dcd05b0fc12915db1013d1d6c1ea4ca34edd9e35
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-09-06 18:47:21 -0500

    Updates

---
+++ precache-manifest.b711f6e3ae1345880d2dbf2c0bad8b76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a4e76a5004893f6371b13adb9efef98",
+    "url": "/message-application/index.html"
+  },
+  {
+    "revision": "c9905224a6bd6a4ea427",
+    "url": "/message-application/static/css/main.86bb5d5b.chunk.css"
+  },
+  {
+    "revision": "1fa489c4b4aecce32cf2",
+    "url": "/message-application/static/js/2.7b361fcd.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/message-application/static/js/2.7b361fcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9905224a6bd6a4ea427",
+    "url": "/message-application/static/js/main.1be37392.chunk.js"
+  },
+  {
+    "revision": "efe45440649e471b7c03",
+    "url": "/message-application/static/js/runtime-main.d7f766cd.js"
+  }
+]);
